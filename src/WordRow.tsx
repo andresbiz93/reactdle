ac9a1c2816b1fc8fc4e2b71812150dc3533479f4
Andres Bizzarri <andres.bizzarri@tridius.co>
2022-07-04 14:27:53 -0500

    added WordRow

---
+++ src/WordRow.tsx
@@ -0,0 +1,33 @@
+const WORD_LENGTH = 5;
+
+interface WordRowProps{
+    letters : string;
+}
+
+function WordRow({letters : lettersProp = ''} : WordRowProps) {
+    const lettersRemaining = WORD_LENGTH - lettersProp.length;
+    const letters = lettersProp.split('').concat(Array(lettersRemaining).fill(''));
+
+    return (
+      <div className="grid grid-cols-5 gap-4">
+       {letters.map((char, idx) => (
+        <CharacterBox key={idx} value={char}/>
+       ))}
+      </div>
+    )
+  }
+
+interface CharacterBoxProps{
+    value:string;
+}
+  
+function CharacterBox({value} : CharacterBoxProps){
+    return (
+        <div className="inline-block border border-gray-500 p-4 uppercase font-bold text-center">
+            {value}
+        </div>
+    );
+}
+
+  export default WordRow
+  
